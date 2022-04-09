0508a6148b3a909764ef7cc60c43d130b6d686af
Rami <rami.awar.ra@gmail.com>
2022-04-10 00:01:34 +0300

    Empty project

---
+++ pydastic/__init__.py
@@ -0,0 +1,19 @@
+# type: ignore[attr-defined]
+"""Pydastic is an elasticsearch python ORM based on Pydantic."""
+
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
